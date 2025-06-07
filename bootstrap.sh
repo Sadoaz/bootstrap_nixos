992bc5f948039293b44e59d2f878e53e0e78974f
Sadoaz <sadoazsosial@gmail.com>
2025-06-07 02:03:50 +0200

    fiks

--- bootstrap.sh
+++ bootstrap.sh
@@ -53,7 +53,7 @@ done
 DEFAULT_DIR="$HOME/nixos-dotfiles"
 REPO_URL="git@github.com:Sadoaz/nixos-dotfiles.git"
 echo "Cloner repo med nix shell: $REPO_URL"
-nix shell nixpkgs#git -c git clone "$REPO_URL" "$DEFAULT_DIR"
+git clone "$REPO_URL" "$DEFAULT_DIR"
 
 SRC_HARDWARE="/etc/nixos/hardware-configuration.nix"
 DEST_HARDWARE="$DEFAULT_DIR/hosts/laptop/hardware-configuration.nix"
@@ -65,15 +65,6 @@ else
   echo "❌ Fant ikke $SRC_HARDWARE – hopper over kopiering"
 fi
 
-# 4. Installer Signal via Flatpak
-if ! flatpak list | grep -q org.signal.Signal; then
-  echo "Installerer Signal fra Flathub..."
-  flatpak install -y flathub org.signal.Signal
-fi
-
-# 5. Sett Flatpak override slik at Signal får tilgang til .XCompose
-sudo flatpak override org.signal.Signal --filesystem=$HOME/.XCompose:ro
-sudo flatpak override org.signal.Signal --env=SIGNAL_PASSWORD_STORE=gnome-libsecret
 
 # 6. Rebuild systemet med flake
 echo "Rebuilder system med flake ~/nixos-dotfiles#laptop"
@@ -91,6 +82,21 @@ if [ -d "$HOME/.local/state/nvim/undo" ]; then
   sudo chown -R "$USER:$(id -gn)" "$HOME/.local/state/nvim/undo"
   chmod 700 "$HOME/.local/state/nvim/undo"
 fi
+
+
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+# 4. Installer Signal via Flatpak
+if ! flatpak list | grep -q org.signal.Signal; then
+  echo "Installerer Signal fra Flathub..."
+  flatpak install -y flathub org.signal.Signal
+fi
+
+# 5. Sett Flatpak override slik at Signal får tilgang til .XCompose
+sudo flatpak override org.signal.Signal --filesystem=$HOME/.XCompose:ro
+sudo flatpak override org.signal.Signal --env=SIGNAL_PASSWORD_STORE=gnome-libsecret
+
+flatpak install flathub com.moonlight_stream.Moonlight
 
 echo
 echo "✅ Alt ferdig. Compose-tast skal nå fungere i Signal også."
